a63ddddc91b11bb3a444c0be38d28c2479f0468b
newecthon <ecthon@gmail.com>
2023-05-18 23:18:41 -0300

    config fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if(!hasLoadedFonts) {
+    return null
+  }
+  
   return (
-    <View className='bg-gray-950 flex-1 items-center justify-center'>
+    <View className='bg-gray-900 flex-1 items-center justify-center'>
       <Text className='text-zinc-50 font-bold text-5xl'>Hello World!</Text>
       <StatusBar style="light" translucent/>
     </View>
